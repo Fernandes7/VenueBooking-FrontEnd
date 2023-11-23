87e290dd000bff180e271c90d860ea923fd4d8de
Ferno <fernandeskj007@gmail.com>
2023-11-23 11:59:10 +0530

    Ipchanged

--- src/API/Api.jsx
+++ src/API/Api.jsx
@@ -1,3 +1,5 @@
-const urls="http://api.venuebooking.cce.edu.in"
-const url="https://ccevenuebooking.onrender.com"
+// const urls="http://api.venuebooking.cce.edu.in"
+// const url="https://ccevenuebooking.onrender.com"
+const url="http://3.111.33.204:8000"
+
 export {url}   
