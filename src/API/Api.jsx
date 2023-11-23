5ea232d2aa24a3fe4d218d1b1db4cdd58caefb08
Ferno <fernandeskj007@gmail.com>
2023-11-23 12:32:35 +0530

    Api sets to Render

--- src/API/Api.jsx
+++ src/API/Api.jsx
@@ -1,5 +1,4 @@
-// const urls="http://api.venuebooking.cce.edu.in"
-// const url="https://ccevenuebooking.onrender.com"
-const url="http://3.111.33.204:8000"
+const url="https://ccevenuebooking.onrender.com"
+//const url="http://3.111.33.204:8000"
 
 export {url}   
